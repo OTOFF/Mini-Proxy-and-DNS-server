e9939f437d794d8533c46400a17af30ff3a26751
OTOFF <mainlieeee@gmail.com>
2024-10-03 07:32:07 +0800

    Add files via upload

---
+++ Proxy.py
@@ -0,0 +1,56 @@
+from socket import*
+import sys
+
+def Proxy(port, fake_ip, server_ip):
+    ListenSocket = socket(AF_INET, SOCK_STREAM)
+    ListenSocket.bind(('', port))
+    ListenSocket.listen(1)
+    print("Proxy is ready to receive on ", port)
+
+    while True:
+
+        connectionSocket, addr = ListenSocket.accept()
+        print(f"Connection established with {addr}")
+
+        primemessage = connectionSocket.recv(2048)
+        decoded = primemessage.decode()
+        try:
+            message = FindMes(decoded)
+        except Exception as e:
+            print("message is not complete.")
+
+        print("Received Message from Client: ", message)
+
+        encodedm = message.encode()
+
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        serverSocket.bind((fake_ip, 0))
+        serverSocket.connect((server_ip, 8080))
+        serverSocket.send(encodedm)
+        print("Sending", message, "to Server: 8080")
+
+        primeresponse = serverSocket.recv(2048)
+        print("Received Message from Server: ", primeresponse.decode())
+        connectionSocket.send(primeresponse)
+
+        if serverSocket:
+            serverSocket.close()
+        if connectionSocket:
+            connectionSocket.close()
+
+def FindMes(message):
+    result = ""
+    if '\n' in message:
+        for i in message:
+            result += i
+            if i == '\n':
+                break
+    else:
+        result = False
+    return result
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    fake_ip = sys.argv[2]
+    server_ip = sys.argv[3]
+    Proxy(port, fake_ip, server_ip)
