50e655aafe784d5599917cd5a2c157a2fd656a65
OTOFF <mainlieeee@gmail.com>
2024-11-09 14:08:37 +0800

    Update Proxy.py

--- Proxy.py
+++ Proxy.py
@@ -6,7 +6,6 @@ import re
 from threading import Thread
 
 throughput ={}
-bitrates_option = {}
 def Proxy(log_file, alpha, port, fake_ip, dnsserver_port):
 ## listen from the client
 
@@ -19,9 +18,10 @@ def Proxy(log_file, alpha, port, fake_ip, dnsserver_port):
             connectionSocket, addr = ListenSocket.accept()
             client_ip = addr[0]
             client_thread = Thread(target=client, args=(connectionSocket, log_file, alpha, client_ip, fake_ip, dnsserver_port))
+            client_thread.daemon = True
             client_thread.start()
 
-        except (OSError, ConnectionRefusedError) as e:
+        except Exception as e:
             break
 
     if ListenSocket:
@@ -32,7 +32,6 @@ def Proxy(log_file, alpha, port, fake_ip, dnsserver_port):
 
 def client(connectionSocket, log_file, alpha, client_ip, fake_ip, dnsserver_port):
     global throughput
-
     try:
         while True:
             primemessage = connectionSocket.recv(2048)
@@ -41,19 +40,12 @@ def client(connectionSocket, log_file, alpha, client_ip, fake_ip, dnsserver_port
                 print("Client disconnected")
                 break
 
-            chunk_name = GetName(primemessage)
-            server_ip = GetIp(dnsserver_port, chunk_name)
+            chunk_name, domain = GetNames(primemessage)
+            server_ip = GetIp(dnsserver_port, domain)
 
-            #when request is manifest file; load in the optional bitrates in the proxy only
-            if "BigBuckBunny_6s.mpd" in chunk_name:
-                manifest_data = FetchManifest(server_ip, chunk_name, fake_ip)
-                bitrates_option[chunk_name] = ParseManifest(manifest_data)
-
-                #send the modified no list mpd to the client
-                no_list = FetchManifest(server_ip, "BigBuckBunny_6s_nolist.mpd", fake_ip)
-                connectionSocket.sendall(no_list)
-                continue
-
+            if not chunk_name or not domain:
+                print("Invalid Request")
+                break
             if not server_ip:
                 print("server IP not found")
                 break
@@ -61,11 +53,25 @@ def client(connectionSocket, log_file, alpha, client_ip, fake_ip, dnsserver_port
             pair_key = (client_ip, server_ip)
             t_current = throughput.get(pair_key, 100)  ##initialization
 
+            manifest_data = FetchManifest(server_ip, "BigBuckBunny_6s.mpd", fake_ip)
+            if not manifest_data:
+                print("Fetching Manifest Data Failed")
+                break
+            
+            bitrates_option = ParseManifest(manifest_data)
+                            
+            if "BigBuckBunny_6s_nolist.mpd" in chunk_name:
+                no_list = FetchManifest(server_ip, chunk_name, fake_ip)
+                if no_list:
+                    connectionSocket.sendall(no_list)
+                continue
+
             #select the certificated bitrate
-            selected_bitrate = SelectBitRate(bitrates_option[chunk_name], t_current)
+            selected_bitrate = SelectBitRate(bitrates_option, t_current)
 
             #modefy the message
             modified_message = Modify(primemessage, selected_bitrate)
+            modified_chunk, _ = GetNames(modified_message)
 
             try:
                 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -88,14 +94,13 @@ def client(connectionSocket, log_file, alpha, client_ip, fake_ip, dnsserver_port
                 t_current = alpha*t_new + (1-alpha) * t_current
                 throughput[pair_key] = t_current
 
-                Log(log_file, duration, t_new, t_current, selected_bitrate, server_ip, chunk_name)
+                Log(log_file, duration, t_new, t_current, selected_bitrate, server_ip, modified_chunk)
 
                 connectionSocket.sendall(primeresponse)
 
             except (OSError, ConnectionRefusedError) as e:
                 serverSocket.close()
                 break
-
     except (OSError, ConnectionRefusedError) as e:
         print("connection failed")
     finally:
@@ -105,23 +110,22 @@ def client(connectionSocket, log_file, alpha, client_ip, fake_ip, dnsserver_port
             serverSocket.close()
 
 
-
-# to capture only the message before EOM
-def GetName(primemessage):
+def GetNames(primemessage):
     try:
         decoded = primemessage.decode()
-        line = decoded.split('\n')[0]
-        chunk_name = line.split(' ')[1]
-        return chunk_name
+        lines = decoded.split('\n')
+        chunk_name = lines[0].split(' ')[1]
+        domain = lines[1].strip().split(' ')[1]
+        return chunk_name, domain
     except IndexError:
-        return "Invalid Message"
+        return None, None
 
 def Log(log_file, duration, tput, avg_tput, chunk_bitrate, server_ip, chunk_name):
     log_time = time.time()
     tput = tput/1000
     avg_tput = avg_tput/1000
     chunk_bitrate = chunk_bitrate/1000
-    result = f"{log_time} {duration} {tput} {avg_tput} {chunk_bitrate} {server_ip} {chunk_name} \n"
+    result = f"{log_time} {duration:.3f} {tput:.2f} {avg_tput:.2f} {chunk_bitrate:.2f} {server_ip} {chunk_name}\n"
     with open (log_file, 'a') as f:
         f.write(result)
         f.flush()
@@ -136,15 +140,21 @@ def GetLength(primeresponse):
     except (ValueError, IndexError):
         return len(primeresponse)
 
-def GetIp(server_port, chunkname):
+def GetIp(server_port, message):
     try:
         dnssocket = socket(AF_INET, SOCK_DGRAM)
-        dnssocket.sendto(chunkname.encode(),('127.0.0.1', server_port))
+        dnssocket.sendto(message.encode('utf-8'),('127.0.0.1', server_port))
         server_ip, _ = dnssocket.recvfrom(1024)
         dnssocket.close()
-        return server_ip.decode()
+        result = Extract(server_ip)
+        if not result:
+            return None
+        else:
+            return result
     except (OSError, ConnectionRefusedError):
         return None
+    finally:
+        dnssocket.close()
 
 def FetchManifest(server_ip, chunk_name, fake_ip):
     #fetch manifest file from the server
@@ -156,17 +166,24 @@ def FetchManifest(server_ip, chunk_name, fake_ip):
         serverSocket.sendall((request.encode))
 
         manifest_data = serverSocket.recv(4096)
-        serverSocket.close()
+        if not manifest_data:
+            return None
         return manifest_data
     except Exception as e:
         return None
+    finally:
+        if serverSocket:
+            serverSocket.close()
     
 def ParseManifest(manifest_data):
     # parese the optional bitrates using regex
     bitrates = []
-    #find all values that indicated by <Representation bandwidth="value">
-    values = re.findall(r'<Representation.*?bandwidth="(\d+)"', manifest_data)
-    bitrates = sorted(int(bandwidth) for bandwidth in values)
+    try:
+        #find all values that indicated by <Representation bandwidth="value">
+        values = re.findall(r'<Representation.*?bandwidth="(\d+)"', manifest_data.decode())
+        bitrates = sorted(int(bandwidth) for bandwidth in values)
+    except Exception as e:
+        print(f"Error parsing manifest: {e}")
     return bitrates
 
 def SelectBitRate(bitrate_options, t_current):
@@ -180,6 +197,21 @@ def Modify(message, bitrate):
     #modify the URI in the request to the selected bitrate label
     modified = re.sub(r"bunny_\d+bps", f"bunny_{bitrate}bps", message.decode())
     return modified.encode()
+
+def Extract(response):
+    try:
+        header = 12
+        question_end = header
+        while response[question_end] != 0:
+            question_end += 1
+        question_end += 1
+        answerin = question_end + 4
+        answerbyte = response[answerin+10: answerin+14]
+        ip = '.'.join(map(str,answerbyte))
+        return ip
+    except IndexError as e:
+        print("DNS response Error")
+        return None
 
 if __name__ == "__main__":
     topo_dir = sys.argv[1]
